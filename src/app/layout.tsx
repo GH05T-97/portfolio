4ead2b9ef9c710f42da8d696d7ddade15e4a1c7b
Zubair <zubair.maqsood6@gmail.com>
2025-08-11 03:57:55 +0100

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-83C46M483K" ></script>
+        <script>
+          {
+            `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-83C46M483K')`
+          }
+        </script>
+      </body>
     </html>
   )
 }
